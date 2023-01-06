d323afe8898386a9802b92190d4401ba3149aa0d
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-06 22:57:47 +0530

    1st commit from recap m56

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from './firebase.init';
+import { useState } from 'react';
+
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () =>{
+    signInWithPopup(auth, provider)
+    .then(result=>{
+      const user = result.user;
+      console.log(user)
+      setUser(user);
+    })
+    .catch(error=>{
+      console.error(error)
+    })
+  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>google sign in</button>
+      <h2>name: {user.displayName}</h2>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYSK8JfSHBXYqAaNlzQU141hCTNjo6PXw",
+  authDomain: "dimple-firebase-recap-m56.firebaseapp.com",
+  projectId: "dimple-firebase-recap-m56",
+  storageBucket: "dimple-firebase-recap-m56.appspot.com",
+  messagingSenderId: "713701436816",
+  appId: "1:713701436816:web:ad0e246dd343ac60f79715"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
